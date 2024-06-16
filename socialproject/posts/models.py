7cb588be940782cecca92fdb90fba169f44a83fa
Sagar231 <sagarfeb298@gmail.com>
2024-06-16 16:57:58 +0530

    added tailwind

---
+++ socialproject/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%y/%m/%d')
+    caption = models.TextField(blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
